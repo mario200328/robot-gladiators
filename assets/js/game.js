f95627fecba3f88c8b7eeeb1d31656fdc653e86b
Mario Balderas <mario000james@gmail.com>
2021-05-17 20:37:26 -0500

    added game over alert

--- assets/js/game.js
+++ assets/js/game.js
@@ -72,10 +72,30 @@ var fight = function(enemyName) {
   }
 };
 
+
+
 for(var i = 0; i < enemyNames.length; i++) {
-  var pickedEnemyName = enemyNames[i];
-  enemyHealth = 50;
-  // call fight function with enemy-robot
+  if (playerHealth > 0) {
+   
+    //let player know what round they are in, remember arrays start at 0 so it needs to have 1 added to it
+    window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+  
+    //pick new enemy to fight based on the index of the enemyNames array
+    var pickedEnemyName = enemyNames[i];
+  
+    // reset enemyHealth before starting new fight
+    enemyHealth = 50;
+  
+    //use debugger to pause script form running and check what's going on at that moment in the code
+    // debugger;
+  
+    // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
     fight(pickedEnemyName);
+  }
+  // if the player isn't alive, stop the game
+  else {
+    window.alert("You have lost your robot in battle! Game Over!");
+    break;
+  }
 }
 
